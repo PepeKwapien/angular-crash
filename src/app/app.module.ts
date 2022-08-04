b639c3cd2123cbed0ea926696d349795ea4b580d
PepeKwapien <pppkwapien@gmail.com>
2022-08-04 19:11:31 +0200

    Added tasks list and task item

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,17 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { ButtonComponent } from './components/button/button.component';
 import { TasksComponent } from './components/tasks/tasks.component';
+import { TaskComponent } from './components/task/task.component';
+import { TaskItemComponent } from './components/task-item/task-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     ButtonComponent,
-    TasksComponent
+    TasksComponent,
+    TaskComponent,
+    TaskItemComponent
   ],
   imports: [
     BrowserModule
